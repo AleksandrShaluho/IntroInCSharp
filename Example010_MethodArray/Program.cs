3e2ed7bf67802f8d0db559b6c049cbd98d125292
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-01 00:29:30 +0300

    Added Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {9,8,6,4,2,11,14,16};
+int ValueToFind = 4;
+int ArrayLength = array.Length;
+int Index = 0;
+
+while (Index < ArrayLength)
+{
+    if (array[Index] == ValueToFind) 
+    {
+        Console.WriteLine(Index);
+        break;
+    }
+    Index++;
+}
+
+
