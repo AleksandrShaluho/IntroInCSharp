880f8be28d386f7b0f9e41aa856d89b991a797ea
Aleksandr Shaluho <sa2sha@mail.ru>
2022-06-30 08:34:05 +0300

    Added Seminars002_IfSquareOfNumber

---
+++ Seminars002_IfSquareOfNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа принимает на вход 2 целых числа и проверяет является ли ВТОРОЕ квадратом первого
+
+Console.Write("Введите первое число: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (SecondNumber == FirstNumber*FirstNumber) Console.WriteLine("a=" + FirstNumber + ";b=" + SecondNumber + "-> ДА");
+else Console.WriteLine("a=" + FirstNumber + ";b=" + SecondNumber + "-> НЕТ");
