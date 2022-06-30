184eb16a9c22f4dc72f99a30860f64efa58fa03c
Aleksandr Shaluho <sa2sha@mail.ru>
2022-06-30 07:19:21 +0300

    Added Example009_IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,10 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+   result = arg1;
+   if (arg2>result) result = arg2;
+   if (arg3>result) result = arg3;
+   return result;
+}
+
+int[] array = {11,22,33,44,55,66,77,88,99};
+
