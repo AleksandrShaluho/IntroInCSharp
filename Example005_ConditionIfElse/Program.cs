3cab2f191bea4f3f67fc33ea6d564cf2675436c8
Aleksandr Shaluho <sa2sha@mail.ru>
2022-06-30 07:12:55 +0300

    Added Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя ");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+} 
+else
+{
+    Console.WriteLine("Привет, " & UserName);
+}
