5340f26f526855768f0250925216cc8d9888c883
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-01 01:26:38 +0300

    Added Seminars004_DeleteSecondDigit

---
+++ Seminars004_DeleteSecondDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿//программа выводит случайное 3хзначное числоБ удаляет второй знак в числе и выводит результат
+
+int Number = new Random().Next(100,1000);
+int CuttedNumber = Number/100*10 + Number%10;
+
+Console.WriteLine($"{Number}->{CuttedNumber}");
+
+//альтернативно
+
+//int FirstDigit = Number/100;
+//int SecondDigit = Number%10;
+
+//Console.WriteLine($"{Number}->{FirstDigit}{SecondDigit}");
