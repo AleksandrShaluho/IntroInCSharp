ae22ffee5004b6d797f808e66c5ea565cd624ebf
Aleksandr Shaluho <sa2sha@mail.ru>
2022-06-30 08:49:19 +0300

    Added Seminars003_DayOfWeek

---
+++ Seminars003_DayOfWeek/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа определяет название дня недели по его номеру (номер генерится случайно)
+
+int NumberOfDay = new Random().Next(1,8);
+
+if (NumberOfDay == 1) Console.WriteLine(NumberOfDay + "-> Понедельник");
+if (NumberOfDay == 2) Console.WriteLine(NumberOfDay + "-> Вторник");
+if (NumberOfDay == 3) Console.WriteLine(NumberOfDay + "-> Среда");
+if (NumberOfDay == 4) Console.WriteLine(NumberOfDay + "-> Четверг");
+if (NumberOfDay == 5) Console.WriteLine(NumberOfDay + "-> Пятница");
+if (NumberOfDay == 6) Console.WriteLine(NumberOfDay + "-> Суббота");
+if (NumberOfDay == 7) Console.WriteLine(NumberOfDay + "-> Воскресенье");
