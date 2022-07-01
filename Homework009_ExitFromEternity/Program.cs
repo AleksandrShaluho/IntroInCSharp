c07904e117d03be6e3a610bd39f57c010ae9ad67
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-02 01:28:16 +0300

    Added Homework009_ExitFromEternity (extra)

---
+++ Homework009_ExitFromEternity/Program.cs
@@ -0,0 +1,10 @@
+﻿//программа с бесконечным циклом. При вводе пользователем слова exit программа заканчивается
+
+string MagicWord = " ";
+
+Console.WriteLine("Welcome to Eternity...Ha-ha-ha-ha-ha...");
+while (2 > 1)
+{
+    MagicWord = Console.ReadLine();
+    if (MagicWord.ToLower() == "exit") break;
+}
