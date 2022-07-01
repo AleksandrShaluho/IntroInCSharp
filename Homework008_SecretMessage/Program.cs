2ae77bfb756871fb028fb43bab459a238b9dd0d6
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-02 01:10:43 +0300

    Added Homework008_SecretMessage (extra)

---
+++ Homework008_SecretMessage/Program.cs
@@ -0,0 +1,21 @@
+﻿//в переменной string есть секретное сообщение, во второй - пароль. Пользователю программы дается 3 попытки ввести пароль и увидеть секретное сообщение.
+
+string Secret = "Поздравляю. Ты - избранный. Тебя выбрала сама Матрица. Если ты будешь и дальше тренироваться, то сможешь зафиксисть все баги Матрицы и стать Мастером ключей";
+
+string Pass = "A#w#xl?oSyjE";
+string NeoPass = " ";
+int MaxAttemptNumber = 3;
+
+while (MaxAttemptNumber > 0)
+{
+    Console.WriteLine($"У тебя есть только {MaxAttemptNumber} попытки, Нео. Вводи пароль: ");
+    Console.CursorVisible = true;
+    NeoPass = Console.ReadLine();
+    if (NeoPass == Pass) 
+    {
+        Console.WriteLine(Secret);
+        break;
+    }
+    MaxAttemptNumber--;
+    if (MaxAttemptNumber == 0) Console.WriteLine("Все кончено, Нео. Агент Смит нас вычислил");
+}
