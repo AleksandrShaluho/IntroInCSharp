564f409374a67cda10acdb49022207f89355611c
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-01 06:38:30 +0300

    Added Seminars005_Multiplicity

---
+++ Seminars005_Multiplicity/Program.cs
@@ -0,0 +1,12 @@
+﻿//программа принимает на вход два числа и выводит, является ли ПЕРВОЕ кратным второму
+
+Console.WriteLine("Введите первое число: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+int Remainder = FirstNumber%SecondNumber;
+
+if (Remainder == 0) Console.WriteLine($"{FirstNumber},{SecondNumber}-> кратно");
+else Console.WriteLine($"{FirstNumber},{SecondNumber}-> не кратно, остаток {Remainder}"); 
