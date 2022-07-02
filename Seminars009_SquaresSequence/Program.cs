14d7285589f6991027e7291d1342238eef8ab344
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-03 00:46:30 +0300

    Added Seminars009_SquaresSequence

---
+++ Seminars009_SquaresSequence/Program.cs
@@ -0,0 +1,14 @@
+﻿//программа принимает на вход число N и выводит последовательность квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число N: ");
+int endNumber = Convert.ToInt32(Console.ReadLine());
+int currentNumber = 1;
+string sequence = "";
+ 
+while (currentNumber <= endNumber)
+{
+    sequence = sequence + currentNumber*currentNumber + " ";
+    currentNumber++;
+}
+Console.WriteLine($"Число - {endNumber}. Последовательность квадратов: {sequence}");
+
