05cb9983275784bcbe1563f28c47c791863f1a62
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-05 23:54:50 +0300

    Added Homework013_Pow

---
+++ Homework013_Pow/Program.cs
@@ -0,0 +1,30 @@
+﻿//программа принимает на вход числа А и В и возводит А в натуральную степень В
+//в решении будем использовать 2 собственных метода: InputInt для ввода числа и Pow - для вычисления степени (все методы в конце текста программы)
+
+//основная часть
+int number = InputInt("Введите число: ");
+int powInt = InputInt("Введите степень в виде натурального числа: ");
+
+//проверка является ли степень натуральным числом и вывод результата
+if (powInt < 0) Console.WriteLine("Степень должна быть натуральным числом");
+else Console.WriteLine($"{number},{powInt} -> {Pow(number,powInt)}");
+
+//метод InputInt позволяет выводить приглашение для ввода и получать число от пользователя
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+//метод Pow вычисляет заданную степень числа
+double Pow(double number, int powNumber)
+{
+   int count = 1;
+   double pow = 1;    
+   while (count <= powNumber)
+   {
+       pow*=number;
+       count++;
+   }
+   return pow;
+}
