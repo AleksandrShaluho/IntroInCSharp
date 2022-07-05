cd0322c843aa444ad5e515ed357fef5d40e26970
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-06 00:10:13 +0300

    Added Homework014_SumOfDigits

---
+++ Homework014_SumOfDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿//программа принимает на вход число и выдает сумму цифр в числк
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int tempNumber = number;
+int sum = 0;
+
+//в каждой итерации цикла определяем последнюю цифру числа путем деления с остатком и прибавляем ее к общей сумме в переменной sum
+//затем отбрасываем отработанный разряд числа целочисленным делением
+while (tempNumber > 1)
+{
+    sum+=tempNumber%10;
+    tempNumber/=10;    
+}
+
+Console.WriteLine(number +"->" + sum);
