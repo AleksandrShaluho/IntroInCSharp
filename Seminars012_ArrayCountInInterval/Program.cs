176c98c03fa94f6be6491503a54983107289db87
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-10 09:03:05 +0300

    Added Seminars012_ArrayCountInInterval

---
+++ Seminars012_ArrayCountInInterval/Program.cs
@@ -0,0 +1,36 @@
+﻿//программа задает одномерный массив из 123 элементов и затем считает количество элементов, значения которых лежат в интервале от 10 до 99 вкючительно.
+
+//основная часть программы
+int[] numbers = new int[123];
+int minIntervalValue = 10;
+int maxIntervalValue = 99;
+int numberOfElements = 0;
+
+FillArray(numbers, 1, 150);
+
+for(int i=0; i<numbers.Length; i++)
+{
+  if (numbers[i]>=minIntervalValue && numbers[i]<=maxIntervalValue)
+    numberOfElements++;  
+}
+PrintArray(numbers);
+Console.WriteLine($"Количество элементов в интервале от {minIntervalValue} до {maxIntervalValue} равно {numberOfElements}");
+
+
+//часть программы, в которой описываются методы
+void FillArray(int[] array, int minValue = -9, int maxValue = 10)
+{
+  for(int i=0; i<array.Length;i++)
+  {
+    array[i] = new Random().Next(minValue, maxValue);
+  }
+}
+
+void PrintArray(int[] array)
+{
+  for(int i=0; i<array.Length;i++)
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
