9c68004fc6c36b46a91b9d24193746d621595965
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-16 08:01:04 +0300

    Added Seminars014_TriangleExist

---
+++ Seminars014_TriangleExist/Program.cs
@@ -0,0 +1,27 @@
+﻿//программа получает на вход три числа - длины сторон треугольника и проверяет может ли существовать треугольник (каждая сторона меньше сумм длин двух других сторон)
+
+int FirstSideLength = InputInt("Введите длину первой стороны ");
+int SecondSideLength = InputInt("Введите длину первой стороны ");
+int ThirdSideLength = InputInt("Введите длину первой стороны ");
+
+if (IsTriangleExist(FirstSideLength, SecondSideLength, ThirdSideLength))
+   Console.WriteLine("Треугольник существует");
+else
+   Console.WriteLine("Треугольник не существует");
+
+
+bool IsTriangleExist (int sideA, int sideB, int sideC)
+{
+if (sideA < (sideB + sideC) &&
+    sideB < (sideA + sideC) &&
+    sideC < (sideA + sideB))
+   return true;
+
+else return false;
+}
+
+int InputInt(string output)
+{
+  Console.WriteLine(output);
+  return Convert.ToInt32(Console.ReadLine());
+}
