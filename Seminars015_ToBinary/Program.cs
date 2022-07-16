040880c14b04a537609ce90205e365fdf84cbc57
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-16 08:23:00 +0300

    Added Seminars015_ToBinary

---
+++ Seminars015_ToBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿//напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+int number = InputInt("Введите целое число ");
+Console.WriteLine("{0} -> {1}", number, DecimalToBinary(number));
+
+
+string DecimalToBinary (int dec)
+{
+  string binary = "";
+  while (dec>0)
+  {
+    binary = dec%2 + binary;
+    dec=dec/2;
+  }
+  return binary;
+}
+
+int InputInt(string output)
+{
+  Console.WriteLine(output);
+  int number = Convert.ToInt32(Console.ReadLine());
+  return number;
+}
