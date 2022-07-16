3a2fc84562a04e56e4f78bd5484f8039cab16bef
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-16 09:09:36 +0300

    Added Seminars016_Fibo

---
+++ Seminars016_Fibo/Program.cs
@@ -0,0 +1,21 @@
+﻿//программа выводит первые N чисел Фибоначчи
+//числа Фибоначчи: каждое следующее равно сумме двух предыдущих. Ноль равен единице, 1 равна единице, потом стандартно.
+
+int number = 3;
+string fibo = String.Empty;
+for(int i = 0; i <= number; i++)
+{
+  if (i == 0)
+  {  fibo = "0 ";
+    continue;
+  }
+  if (i == 1)
+  { 
+    fibo = fibo + "1";
+    continue;
+  }
+  fibo = fibo + " " + ((i-1)+(i-2));
+  
+}
+Console.WriteLine(fibo);
+
