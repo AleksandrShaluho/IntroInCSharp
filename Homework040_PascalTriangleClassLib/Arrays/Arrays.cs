25ebd14e48bfc49730fdd5e8a1dfe538f002961f
Aleksandr Shaluho <sa2sha@mail.ru>
2022-07-29 00:15:46 +0300

    Added Homework040_PascalTriangleClassLib

---
+++ Homework040_PascalTriangleClassLib/Arrays/Arrays.cs
@@ -0,0 +1,43 @@
+﻿namespace Arrays;
+public class PascalTriangle
+{
+    public int cellWidth = 3;
+    int[,] triangle;
+    public int size = 5;
+    public void Fill()
+    {
+        triangle = new int[size, size];
+        triangle[0, 0] = 1;
+
+        for (int i = 1; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (j - 1 < 0)
+                    triangle[i, j] = 1;
+                else
+                    triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+            }
+        }
+    }
+
+    public void Print()
+    {
+        Console.Clear();
+        int columns = triangle.GetLength(1);
+        int columnAlign;
+        for (int i = 0; i < triangle.GetLength(0); i++)
+        {
+            columnAlign = (cellWidth * (columns - i)) / 2;
+            for (int j = 0; j < columns; j++)
+            {
+                Console.SetCursorPosition(columnAlign, i);
+                if (triangle[i, j] != 0)
+                    Console.Write(triangle[i, j]);
+                columnAlign += cellWidth;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Homework040_PascalTriangleClassLib/Program.cs
@@ -0,0 +1,25 @@
+﻿//Вывести первые N строк треугольника Паскаля. Сделать вывод в виде равнобедренного треугольника
+//треугольник Паскаля имеет свойства: на вершине и по бокам стоят единицы, а каждое число равно сумме расположенных над ним чисел
+
+using Arrays;
+
+
+namespace ArraysProject
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            PascalTriangle triangle = new();
+            Console.WriteLine("Задайте количество строк треугольника Паскаля для вывода: ");
+            triangle.size =Int32.Parse(Console.ReadLine());
+	    triangle.Fill();
+	    if (triangle.size < 15 && triangle.size >= 10)
+		    triangle.cellWidth = 5;
+	    else if (triangle.size >= 15)
+		    triangle.cellWidth = 8;
+		    
+	    triangle.Print();
+        }
+    }
+}
